6ad4d1c8a0645c3ac74c43696a5c910315ab49fb
josephc <sherlock@ultimatesoftware>
2015-04-22 10:18:49 -0400

    log regid

--- www/js/dist/person-app.js
+++ www/js/dist/person-app.js
@@ -328,11 +328,10 @@ function onNotificationGcm(e) {
     switch (e.event) {
         case 'registered':
             if (e.regid.length > 0) {
-                $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
-
                 // Your GCM push server needs to know the regID before it can push to this device
                 // here is where you might want to send it the regID for later use.
                 console.log("regID = " + e.regid);
+                alert("regID = " + e.regid);
             }
             break;
 
@@ -340,28 +339,26 @@ function onNotificationGcm(e) {
             // if this flag is set, this notification happened while we were in the foreground.
             // you might want to play a sound to get the user's attention, throw up a dialog, etc.
             if (e.foreground) {
-                $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
-
                 // if the notification contains a soundname, play it.
                 alert("Ultimate + GO + Push Notifications == AWESOME" + e.payload.message);
             } else {
                 if (e.coldstart) {
-                    $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+                    console.log('<li>--COLDSTART NOTIFICATION--' + '</li>');
                 } else {
-                    $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+                    console.log('<li>--BACKGROUND NOTIFICATION--' + '</li>');
                 }
             }
 
-            $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-            $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+            console.log('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+            console.log('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
             break;
 
         case 'error':
-            $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+            console.log('<li>ERROR -> MSG:' + e.msg + '</li>');
             break;
 
         default:
-            $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+            console.log('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
             break;
     }
 };App.Router.map(function() {
